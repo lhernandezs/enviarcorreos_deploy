9aa050d8a27ccc1444e56e5e816c851598ec0ac9
lhernandezs <lhernandezs331@gmail.com>
2023-07-12 17:27:30 -0500

    5C 11 jul

--- modelo.py
+++ modelo.py
@@ -1,13 +1,10 @@
-from datetime import date
 from pydantic import BaseModel
-from pydantic import Field
 from pydantic import EmailStr
-#from typing import List
 
 class Modelo(BaseModel):
 
     instructor: str
     email: EmailStr
     fichas: list
-    show_information: bool = False
+
 
