f250acefad98b5df0314f3f4adbe8e39cfb92d63
lhernandezs <lhernandez331@gmail.com>
2023-08-21 18:29:33 -0500

    2C 21 ago

--- datos.py
+++ datos.py
@@ -33,34 +33,21 @@ class Datos:
         credenciales = os.path.join(self._path, self._credenciales)
         creds = None
 
-        try:
-            
-            if os.path.exists(token):
-                creds = Credentials.from_authorized_user_file(token, self._scopes)
+        if os.path.exists(token):
+            creds = Credentials.from_authorized_user_file(token, self._scopes)
 
         # Si no hay credenciales (válidas) disponibles, permite que el usuario inicie sesión.
-
-            if not creds or not creds.valid:
-                if creds and creds.expired and creds.refresh_token:
-                    creds.refresh(Request())
-                else:
-                    flow = InstalledAppFlow.from_client_secrets_file(credenciales, self._scopes)
-                    creds = flow.run_local_server(port=0)
-                
-                # Guarda las credenciales para la próxima ejecución
-                with open(token, 'w') as token:
-                    token.write(creds.to_json())
-        except:
-            remove(token)
-            creds = Credentials.from_authorized_user_file(token, self._scopes)
-            creds.refresh(Request())
-
-            flow = InstalledAppFlow.from_client_secrets_file(credenciales, self._scopes)
-            creds = flow.run_local_server(port=0)
-
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(credenciales, self._scopes)
+                creds = flow.run_local_server(port=0)
+            
+            # Guarda las credenciales para la próxima ejecución
             with open(token, 'w') as token:
                 token.write(creds.to_json())
-
+        
         return creds
 
     # obtengo los datos de las fichas o None si no es posible la conexión
